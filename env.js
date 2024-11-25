3dfaa29f07583563d860a74a9896fcdf8a360fed
= <=>
2024-11-25 17:14:29 -0300

    change map API key

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 const ENVIRONMENT = {
-  MAPS_API_KEY: 'AIzaSyCgogrnISz9ccFj6kNFkYo7Fc22UnU3Jkc',
+  MAPS_API_KEY: 'AIzaSyDVe3b6RjqX_VIKvr5p3yN4mnAHz_NkAlQ',
   MAP_ID: 'af30275f3be96bf9',
   CAMPARI_PLACES_URL: 'https://api.gratisspritz.com/places',
   CAMPARI_SUBSCRIPTION_URL: 'https://api.gratisspritz.com/subscribe'

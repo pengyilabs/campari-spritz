274d15879d26b1983e56d360e47d6d2632b1668a
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2024-11-30 14:36:20 -0300

    fix typo

--- src/components/successModal.js
+++ src/components/successModal.js
@@ -7,7 +7,7 @@ export const renderSuccessModal = (place) => {
         <img src="./src/assets/icons/cup-modal-icon.png" >
       </figure>
       <div class="result-modal-content">
-        <h2 class="result-modal-title">YOUR'RE ALL SET!</h2>
+        <h2 class="result-modal-title">YOU'RE ALL SET!</h2>
         <div class="result-modal-text">
           <p class="margin-bottom-p">We’ve sent your voucher to <span class="result-modal-email">${state.userEmail}</span></p>
           <p>Check your inbox for your QR code and bring it to <span class="result-modal-place-name">${place.name}</span> to claim your free Campari Spritz!</p>
@@ -16,4 +16,4 @@ export const renderSuccessModal = (place) => {
       </div>
     </div>
   `;
-}
+};

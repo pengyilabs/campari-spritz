d5a45b9510cd4c910d0c4fac39e25673920349a3
Nazareno Altamiranda <nazareno.anzar@gmail.com>
2025-02-13 22:28:11 -0300

    env variables

---
+++ env.Production.js
@@ -0,0 +1,9 @@
+const ENVIRONMENT = {
+  MAPS_API_KEY: 'AIzaSyABJx0aseXXxxaTPEfw_cn0yoA4tIxQHTE',
+  MAP_ID: 'af30275f3be96bf9',
+  CAMPARI_PLACES_URL: 'https://api.gratisspritz.com/places',
+  CAMPARI_PLACES_DETAILS_URL: 'https://api.gratisspritz.com/placeDetails',
+  CAMPARI_SUBSCRIPTION_URL: 'https://api.gratisspritz.com/subscribe'
+};
+
+export default ENVIRONMENT;
---
+++ env.upwork.js
@@ -0,0 +1,8 @@
+const ENVIRONMENT = {
+  MAPS_API_KEY: 'AIzaSyDVe3b6RjqX_VIKvr5p3yN4mnAHz_NkAlQ',
+  MAP_ID: 'af30275f3be96bf9',
+  CAMPARI_PLACES_URL: 'https://api.gratisspritz.com/places',
+  CAMPARI_SUBSCRIPTION_URL: 'https://api.gratisspritz.com/subscribe'
+};
+
+export default ENVIRONMENT;

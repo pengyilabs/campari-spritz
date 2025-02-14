774bb670fa4017104a770976538e2145e4aadb98
Nazareno Altamiranda <nazareno.anzar@gmail.com>
2025-02-14 19:15:13 -0300

    testing: reverse changes

--- src/components/barList.js
+++ src/components/barList.js
@@ -25,14 +25,14 @@ export const insertBarListMobileLogic = () => {
   const expandList = () => {
     isExpanded = true;
     swipeContainer.style.top = POSITIONS.EXPANDED;
-    // barList.style.overflowY = "scroll";
+    barList.style.overflowY = "scroll";
     findABarSection.classList.add("shadow-bottom-hide");
   };
 
   const collapseList = () => {
     isExpanded = false;
     swipeContainer.style.top = POSITIONS.COLLAPSED;
-    // barList.style.overflowY = "hidden";
+    barList.style.overflowY = "hidden";
     findABarSection.classList.remove("shadow-bottom-hide");
   };
 
